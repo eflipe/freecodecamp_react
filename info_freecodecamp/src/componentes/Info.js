2ad69c76f0bc2b013bddb4ebba9a6559b27e5d8f
eflipe <felipecabaleiro@gmail.com>
2022-09-21 16:19:55 -0300

    [ADD] Asignar estilos CSS

--- info_freecodecamp/src/componentes/Info.js
+++ info_freecodecamp/src/componentes/Info.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Info.css';
 
 // componente funcional
 export function Info() {
@@ -11,7 +12,12 @@ export function Info() {
       <div className="contenedor-texto-info">
         <p className="nombre-info"> Nombre </p>
         <p className="cargo-info">Ocupación</p>
-        <p className="text-info">Blah blah blah</p>
+        <p className="text-info">Excepteur aut dolore iudicem, velit fidelissimae cernantur cillum singulis.
+        Ipsum litteris in quem illum. Malis nostrud fidelissimae.Velit iis quamquam in
+        cernantur labore multos in legam. Quamquam te constias, possumus graviterque se
+        quibusdam sed quae si eiusmod eu noster, export aliquip sed praetermissum. 
+        Senserit malis arbitror appellat sed aut id despicationes id quis litteris qui
+        labore duis, expetendis relinqueret ad ingeniis.</p>
       </div>
 
     </div>
