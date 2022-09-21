2c16116ff48b17950a05cc48cb6e966417c5874e
eflipe <felipecabaleiro@gmail.com>
2022-09-21 17:05:02 -0300

    [ADD] mas componentes

--- info_freecodecamp/src/App.js
+++ info_freecodecamp/src/App.js
@@ -16,7 +16,22 @@ function App() {
           empresa='Acuarela'
           texto='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
           />
-
+        <Info
+          nombre='Van Gogh'
+          pais='Países Bajos'
+          imagen='2'
+          ocupacion='Pintor'
+          empresa='Acuarela'
+          texto='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+          />
+        <Info
+          nombre='Van Gogh'
+          pais='Países Bajos'
+          imagen='3'
+          ocupacion='Pintor'
+          empresa='Acuarela'
+          texto='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+          />
       </div>
     </div>
   );
