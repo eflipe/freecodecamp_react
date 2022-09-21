1ed97a454c3872f606091218099573fa58fccc93
eflipe <felipecabaleiro@gmail.com>
2022-09-20 23:00:24 -0300

    [MOD] mostrar componentes

--- info_freecodecamp/src/App.js
+++ info_freecodecamp/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Info from './componentes/Info.js';
 
+// renderiza el componente para mostrarlo
 function App() {
   return (
     <div className="App">
-
+      <Info />
     </div>
   );
 }
--- info_freecodecamp/src/componentes/Info.js
+++ info_freecodecamp/src/componentes/Info.js
@@ -19,3 +19,5 @@ function Info() {
   );
 
 }
+
+export default Info;
