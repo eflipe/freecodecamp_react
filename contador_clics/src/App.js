a4f8a8a8c26091e501b280793cba53efb41bd873
eflipe <felipecabaleiro@gmail.com>
2022-09-22 09:34:51 -0300

    [ADD] importar img

--- contador_clics/src/App.js
+++ contador_clics/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import freeCodeCampLogo from './src/img/freecodecamp-logo.png'
+
 
 function App() {
   return (
     <div className="App">
+      <div className="logo-contenedor">
+        <img className="logo" src={freeCodeCampLogo} alt="Logo" />
+
+      </div>
 
     </div>
   );
