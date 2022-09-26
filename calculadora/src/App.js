012df43ebb9c3f150b753349fcf4b6e56f356065
eflipe <felipecabaleiro@gmail.com>
2022-09-26 11:09:34 -0300

    [ADD] componente y estilos BotonClear

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Boton from './componentes/Boton';
 import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
 
 function App() {
   return (
@@ -38,7 +39,9 @@ function App() {
         <Boton>/</Boton>
         </div>
         <div className="fila">
-
+        <BotonClear>
+          Clear
+        </BotonClear>
         </div>
       </div>
 
---
+++ calculadora/src/componentes/BotonClear.jsx
@@ -0,0 +1,12 @@
+import "../estilos/BotonClear.css";
+
+
+const BotonClear = (props) => (
+  <div className='boton-clear'>
+    {props.children}
+  </div>
+
+
+);
+
+export default BotonClear;
