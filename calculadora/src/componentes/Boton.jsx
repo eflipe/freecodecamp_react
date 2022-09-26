5f103cf967c95db6ee7a35bb895ff4cf48be705a
eflipe <felipecabaleiro@gmail.com>
2022-09-26 12:17:37 -0300

    [ADD] funcionalidad Boton

--- calculadora/src/componentes/Boton.jsx
+++ calculadora/src/componentes/Boton.jsx
@@ -10,6 +10,7 @@ function Boton(props) {
   return (
     <div
       className={`boton-contenedor ${esOperador(props.children) ? "operador" : ''}`.trimEnd()}
+      onClick={() => props.manejarClic(props.children)}
       >
       {props.children}
     </div>
